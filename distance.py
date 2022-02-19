4963e01533b45945cc44feb6858f885a1013cd75
David Stansby <dstansby@gmail.com>
2022-02-19 23:25:26 +0000

    Add distance along track

---
+++ distance.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def distance(*, lon, lat):
+    """
+    Calculate the great circle distance in kilometers between a set of points
+    on the earth (specified in decimal degrees).
+    """
+    lon = np.deg2rad(lon)
+    lat = np.deg2rad(lat)
+
+    # haversine formula
+    dlon = lon[1:] - lon[:-1]
+    dlat = lat[1:] - lat[:-1]
+    a = np.sin(dlat/2)**2 + np.cos(lat[:-1]) * np.cos(lat[:-1]) * np.sin(dlon/2)**2
+    c = 2 * np.arcsin(np.sqrt(a))
+    r = 6371  # Radius of earth in kilometers
+    return np.sum(c) * r
--- generate_track.py
+++ generate_track.py
@@ -1,34 +1,55 @@
-from geopy.distance import geodesic
+import itertools
+
+from geopy.distance import great_circle
 import geopandas as gpd
 import numpy as np
 
+from bokeh.io import export_png
+from bokeh.models import ColumnDataSource, PanTool, WheelZoomTool
 from bokeh.plotting import figure, show
-from bokeh.models import ColumnDataSource
+
+from distance import distance
 
 track = gpd.read_file('SW_Coast_Path_Full.json')
 track = track.sort_values('title')
 
+pan = PanTool()
+wheel = WheelZoomTool()
+
 fig = figure(x_axis_type="mercator", y_axis_type="mercator",
              tooltips=[('Section', '$name'),
-                       ('Distance', '@distance')])
-fig.aspect_ratio = 1
+                       ('Distance', '@distance')],
+             tools=[pan, wheel],
+             toolbar_location=None,
+             plot_width=600,
+             plot_height=300)
+fig.aspect_scale = 1
 fig.match_aspect = True
 fig.axis.visible = False
+
+fig.xgrid.grid_line_color = None
+fig.ygrid.grid_line_color = None
+fig.outline_line_width = 2
+fig.outline_line_color = 'black'
+
+fig.toolbar.active_scroll = wheel
+fig.toolbar.active_drag = pan
+colors = itertools.cycle(['#e41a1c', '#377eb8', '#4daf4a'])
+
 
 for idx, t in track.iterrows():
     geom = t.geometry
     if geom is not None:
         coords = geom.coords
-        distances = [geodesic(t0, t1).km for t0, t1 in
-                     zip(coords[:-1], coords[1:])]
-        distance = np.sum(distances)
-        distance = "{:0.2f} km".format(distance)
-
         lon = [c[0] for c in coords]
         lat = [c[1] for c in coords]
+
+        d = "{:0.2f} km".format(distance(lon=lon, lat=lat))
         source = ColumnDataSource({'x': lon,
                                    'y': lat,
-                                   'distance': [distance] * len(lon)})
-        fig.line('x', 'y', line_width=1, name=t['title'], source=source)
+                                   'distance': [d] * len(lon)})
+        fig.line('x', 'y', line_width=1.5, name=t['title'], source=source,
+                 color=next(colors))
 
+# export_png(fig, filename='map.png', width=1200, height=600)
 show(fig)
